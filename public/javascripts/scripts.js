d429f5aa755524dc8ee5184171b23638a0b341d7
PierreDV <p.verteuil@gmail.com>
2018-12-10 17:30:46 +0100

    added mobile responsive menu

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('js--nav-icon ion-icon')
+    nav.slideToggle(200);
+    // if (icon.attr('name') === 'menu') {
+    //   icon.attr('name') = 'close';
+    // } else {
+    //   icon.attr('name') = 'menu';
+    // }
+  });
+
+});
