118ab621f1e35a0ab564fd3a448e2047a9d6e476
PierreDV <p.verteuil@gmail.com>
2018-12-19 16:30:03 +0100

    add footer and change nav icon symbol

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-
   $('.js--nav-icon').click(function() {
     var nav = $('.js--main-nav');
-    var icon = $('js--nav-icon ion-icon')
+    var icon = $('#nav-icon')
     nav.slideToggle(200);
-    // if (icon.attr('name') === 'menu') {
-    //   icon.attr('name') = 'close';
-    // } else {
-    //   icon.attr('name') = 'menu';
-    // }
-  });
 
+    if(icon.attr('name') == 'menu') {
+      icon.attr('name', 'close');
+    } else {
+      icon.attr('name', 'menu');
+    }
+  });
 });
