c73469f6ea314e2aade5e4d78f7b9637cb20d933
PierreDV <p.verteuil@gmail.com>
2023-01-02 11:44:55 +0100

    WIP map fix

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,15 +4,25 @@ function centerLeafletMapOnMarker(map, marker) {
   map.fitBounds(markerBounds);
 }
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+// var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiZGV2ZXJ0ZXUiLCJhIjoiY2pwZmkycmEyMDNnODNxcHUyMms0cXBjbyJ9.RZYyAgc8gLrvDuQ_OFGIlQ'
-}).addTo(mymap);
+// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     id: 'mapbox.streets',
+//     accessToken: 'pk.eyJ1IjoiZGV2ZXJ0ZXUiLCJhIjoiY2pwZmkycmEyMDNnODNxcHUyMms0cXBjbyJ9.RZYyAgc8gLrvDuQ_OFGIlQ'
+// }).addTo(mymap);
 
-var marker = L.marker([46.415750, 6.268020]).addTo(mymap);
-marker.bindPopup("<b>Titan Fitness</b><br>6 Route des Avouillons, 1196, Gland").openPopup();
-centerLeafletMapOnMarker(mymap, marker);
+// var marker = L.marker([46.415750, 6.268020]).addTo(mymap);
+// marker.bindPopup("<b>Titan Fitness</b><br>6 Route des Avouillons, 1196, Gland").openPopup();
+// centerLeafletMapOnMarker(mymap, marker);
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+var marker = L.marker([46.415750, 6.268020]).addTo(map)
+    .bindPopup("<b>Titan Fitness</b><br>6 Route des Avouillons, 1196, Gland")
+    .openPopup();
+
+centerLeafletMapOnMarker(map, marker);
